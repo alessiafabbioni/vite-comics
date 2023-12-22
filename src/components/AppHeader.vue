49f274335cccd7db0a29984b67a0fd62acfa0f53
alessiafabbioni <alessiafabbioni@gmail.com>
2023-12-22 16:07:13 +0100

    Add: App header nav menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,19 +7,37 @@ export default{
 <template>
   <header>
     <a href="/">
-        <img src=".public/img/ds-logo.png" alt="DC Logo">
+        <img src="/img/dc-logo.png" alt="DC Logo">
     </a>
+    <ul>
+        <li><a href="/">CHARACTERS</a></li>
+        <li><a href="/">COMICS</a></li>
+        <li><a href="/">MOVIES</a></li>
+        <li><a href="/">TV</a></li>
+        <li><a href="/">GAMES</a></li>
+        <li><a href="/">COLLECTIBLES</a></li>
+        <li><a href="/">VIDEOS</a></li>
+        <li><a href="/">FANS</a></li>
+        <li><a href="/">NEWS</a></li>
+        <li><a href="/">SHOP</a></li>
+    </ul>
 
 
   </header>
 </template>
 
 <style lang="scss" scoped>
-@use './styles/partials/variables.scss' as *;
-@use './styles/mixins.scss' as *;
+@use '../styles/partials/variables.scss' as *;
+@use '../styles/partials/mixins.scss' as *;
 
 header {
     text-align: center;
+    @include space-between();
+
+    a {
+        padding: 1rem;
+
+    }
 
     ul {
         list-style: none;
