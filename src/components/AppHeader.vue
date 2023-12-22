a8b0d2653b2d9b914abc0c4bc72e9d5bc99834f3
alessiafabbioni <alessiafabbioni@gmail.com>
2023-12-22 15:50:03 +0100

    Add: AppHeader image management

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,47 @@
+<script>
+export default{
+    name: "AppHeader"
+}
+</script>
+
+<template>
+  <header>
+    <a href="/">
+        <img src=".public/img/ds-logo.png" alt="DC Logo">
+    </a>
+
+
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use './styles/partials/variables' as *;
+@use './styles/mixins' as *;
+
+header {
+    text-align: center;
+
+    ul {
+        list-style: none;
+        margin: 2.5rem 1rem 4rem;
+        @include center();
+
+        li a{
+            display: inline-block;
+            padding: 1rem;
+            text-decoration: none;
+            font-weight: 600;
+            transition: color 0.5s;
+
+            &.active,
+            &:hover {
+                color: $secondary;
+                border-bottom: solid 3px $secondary; 
+            }
+
+
+        }
+    };
+}
+
+</style>
