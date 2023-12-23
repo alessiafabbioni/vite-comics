7575b6b50bc1f28fce9a24e9410a7362f85f7026
alessiafabbioni <alessiafabbioni@gmail.com>
2023-12-23 11:14:49 +0100

    FIX: footer menu

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
     AppHeader,
     ListProducts,
     PageMenu,
-    FooterMenu
+    FooterMenu,
   }
 }
 </script>
--- src/components/FooterMenu.vue
+++ src/components/FooterMenu.vue
@@ -1,12 +1,40 @@
 <script>
 export default{
-    name: "FooterMenu"
+    name: "FooterMenu",
+    data() {
+        return {
+            footerNavs: [
+                {
+                    section: 'DC COMICS',
+                    links: ['characters', 'comics', 'movies', 'tv', 'games', 'videos', 'news']
+                },
+                {
+                    section: 'SHOP',
+                    links: ['shop dc', 'shop dc collectibles']
+                },
+                {
+                    section: 'DC',
+                    links: ['terms of use', 'privacy policy', 'ad choices', 'advertising', 'jobs', 'subscriptions', 'talent workshops', 'CPSC certificates', 'ratings', 'shop help', 'contact us']
+                },
+                {
+                    section: 'SITES',
+                    links: ['DC', 'Mad magazine', 'DC kids', 'DC Universe', 'DC power visa']
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <template>
-    <footer>
-        
+    <footer class="row">
+        <nav class="col" v-for="footerNav in footerNavs" :key="footerNav.section">
+            <h3>{{ footerNav.section }}</h3>
+            <ul>
+                <li v-for="link in footerNav.links" :key="link">{{ link }}</li>
+            </ul>
+        </nav>
+
 
     </footer>
 </template>
@@ -16,7 +44,33 @@ export default{
 @use '../styles/partials/mixins.scss' as *;
 
 footer {
-    background-image: url(".public/img/footer-bg.jpg");
+    background-color: black;
+    background-image: url("./img/footer-bg.jpg");
+
+}
+nav {
+
+    h3 {
+        color: white;
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    li {
+        color: gray;
+        padding: 5px 0;
+    }
+}
+
+.row {
+    display: flex;
+    flex-direction: row;
+}
+
+.col{
+    margin: 1rem;
 }
 
 </style>
